7c5491c312b83e88491ff522c354c0279edb2aea
Dallas Johnson <dallasj001@gmail.com>
2019-05-11 16:15:04 +0100

    Remove all contents

--- output/jungle/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CONTRACT='dacproposals'
-
-eosio-cpp -o output/jungle/$CONTRACT/$CONTRACT.wasm $CONTRACT.cpp
-eosio-abigen $CONTRACT.hpp -contract $CONTRACT -output output/jungle/$CONTRACT/$CONTRACT.abi
--- tests/contract-shared-dependencies/daccustodian_stub_compile.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CONTRACT='daccustodian'
-
-eosio-cpp -o $CONTRACT/$CONTRACT.wasm ./daccustodian_stub/daccustodian_stub.cpp
-eosio-abigen ./daccustodian_stub/daccustodian_stub.hpp -contract $CONTRACT -output $CONTRACT/$CONTRACT.abi
